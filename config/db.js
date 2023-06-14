37d3c8a38968dd156b563144de434f12bef71308
leolanese <laneseleo@gmail.com>
2023-06-14 17:37:23 +0100

    improvements

--- config/db.js
+++ config/db.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
 
-const connectDB = async() => {
+/**
+ * Connect to MongoDB
+ * @param {boolean} exitOnError Whether to exit the process if a connection error occurs
+ * @returns {Promise<void>}
+ */
+const connectDB = async (exitOnError = true) => {
+    const { MONGO_URI } = process.env;
+
+    if (!MONGO_URI) {
+        throw new Error("Environment variable MONGO_URI is not set");
+    }
+
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
-        })
-        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+        });
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.blue.underline);
     } catch (error) {
-        console.error(`Error : ${error.message}`.red.underline.bold)
-        process.exit(1)
+        console.error('Error connecting to MongoDB'.red.underline.bold, error);
+        
+        if (exitOnError) {
+            process.exit(1);
+        }
     }
 }
 
