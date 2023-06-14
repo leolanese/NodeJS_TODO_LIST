28b905700890369d4b1f3ee496576b7f79516bb0
leolanese <laneseleo@gmail.com>
2023-06-14 17:47:23 +0100

    improvements

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
+// Importing express module for creating server.
 const express = require('express');
+
+// Importing dotenv module for using environment variables in the application.
 const dotenv = require('dotenv');
+
+// Importing colors module for console logging with colors.
 const colors = require('colors');
+
+// Importing morgan module, which is a HTTP request logger middleware.
 const morgan = require('morgan');
+
+// Importing built-in path module, which provides utilities for working with file and directory paths.
 const path = require('path');
+
+// Importing a custom module for routes related to 'todo' functionality.
 const todoRoute = require('./routes/todo');
 
+// Importing a custom module to connect to the MongoDB database.
 const connectDB = require('./config/db');
-dotenv.config({ path: './env'})
-connectDB()
 
+// Using the config method of dotenv to set the path of the environment variables file.
+dotenv.config({ path: './env'});
+
+// Call the connectDB function to establish the database connection.
+connectDB();
+
+// Creating an express application.
 const app = express();
+
+// Use express's inbuilt JSON parser.
 app.use(express.json())
 
-if(process.env.MODE === 'development'){
+// If the application mode is 'development', use morgan for logging HTTP requests.
+if (process.env.MODE === 'development'){
     app.use(morgan('dev'))
 }
+
+// Set the port number for the server to listen on. Default is 5000 if not specified in the environment variables.
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/task', todoRoute)
+// Use the 'todoRoute' for handling API requests under '/api/task'.
+//app.use('/api/task', todoRoute)
 
+// Route for the root ('/') of the application. When this route is hit, it sends back a message.
 app.get('/', (req, res) => {
-    res.send('API is running good')
+    res.send('YAY! API is running good')
 })
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold));
+// Start the server and listen on the specified port. Log a message to console when server starts.
+app.listen(PORT, console.log(`Server is running on port: ${PORT}`.green.bold));
